46b5bf581b91c17dd79cb53be2285c45920fdc0c
yuxingdong <616106356@qq.com>
2018-07-21 20:32:48 +0800

    add plugin

---
+++ plugin/src/main/groovy/com/robog/MethodLogExtension.groovy
@@ -0,0 +1,13 @@
+package com.robog
+
+class MethodLogExtension {
+  def enabled = true
+
+  def setEnabled(boolean enabled) {
+    this.enabled = enabled
+  }
+
+  def getEnabled() {
+    return enabled;
+  }
+}
